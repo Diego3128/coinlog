7668fb5495eb96194edc2ea7fa99ae2ccf347b3d
DiegoFabian <145170018+Diego3128@users.noreply.github.com>
2026-08-10 13:51:29 -0500

    fix: updated interface IBudgetService & implementation - interface & method signs were updated to match the repository return

--- backend/src/services/budget.service.ts
+++ backend/src/services/budget.service.ts
@@ -2,32 +2,37 @@ import { CreateBudgetDto } from "../dtos/budget/create-budget.dto";
 import Budget from "../models/Budget";
 import { IBudgetRepository } from "../repositories/interfaces/budget.repository.interface";
 import { IBudgetService } from "./interfaces/budget.service.interface";
-import { FilterBudgetDto } from '../dtos/budget/filter-budget.dto';
+import { FilterBudgetDto } from "../dtos/budget/filter-budget.dto";
 
 export class BudgetService implements IBudgetService {
+  private readonly budgetRepository: IBudgetRepository;
+  // DI: IBudgetRepository
+  public constructor(budgetRepository: IBudgetRepository) {
+    this.budgetRepository = budgetRepository;
+  }
 
-    private readonly budgetRepository: IBudgetRepository
-    // DI: IBudgetRepository
-    public constructor(
-        budgetRepository: IBudgetRepository
-    ) {
-        this.budgetRepository = budgetRepository;
-    }
+  createBudget = async (data: CreateBudgetDto): Promise<Budget> => {
+    return await this.budgetRepository.createBudget(data);
+  };
 
-    createBudget = async (data: CreateBudgetDto): Promise<Budget> => {
-        return await this.budgetRepository.createBudget(data);
-    }
+  getAllBudgets = async (
+    filterDto: FilterBudgetDto,
+  ): Promise<{
+    data: Budget[];
+    pagination: {
+      count: number;
+      totalCount: number;
+      page: number;
+      totalPages: number;
+      limit: number;
+    };
+  }> => {
+    //business logic
+    return await this.budgetRepository.getAllBudgets(filterDto);
+  };
 
-    getAllBudgets = async (filterDto: FilterBudgetDto): Promise<{ rows: Budget[];count: number;}> => {
-        //business logic
-        return await this.budgetRepository.getAllBudgets(filterDto)
-    }
+  getBudgetById: (id: number) => Promise<Budget | null>;
 
-    getBudgetById: (id: number) => Promise<Budget | null>;
-
-    updateBudgetById: (id: number, data?: any) => Promise<Budget | null>;
-    deleteBudgetById: (id: number) => Promise<Budget>;
-
-
-
+  updateBudgetById: (id: number, data?: any) => Promise<Budget | null>;
+  deleteBudgetById: (id: number) => Promise<Budget>;
 }
--- backend/src/services/interfaces/budget.service.interface.ts
+++ backend/src/services/interfaces/budget.service.interface.ts
@@ -3,9 +3,18 @@ import { FilterBudgetDto } from "../../dtos/budget/filter-budget.dto";
 import Budget from "../../models/Budget";
 
 export interface IBudgetService {
-    getAllBudgets: (filterDto: FilterBudgetDto) => Promise<{ rows: Budget[];count: number;}> ;
-    createBudget: (data: CreateBudgetDto) => Promise<Budget>;
-    getBudgetById: (id: number) => Promise<Budget | null>;
-    updateBudgetById: (id: number, data?: any) => Promise<Budget | null>
-    deleteBudgetById: (id: number) => Promise<Budget>
+  getAllBudgets: (filterDto: FilterBudgetDto) => Promise<{
+    data: Budget[];
+    pagination: {
+      count: number;
+      totalCount: number;
+      page: number;
+      totalPages: number;
+      limit: number;
+    };
+  }>;
+  createBudget: (data: CreateBudgetDto) => Promise<Budget>;
+  getBudgetById: (id: number) => Promise<Budget | null>;
+  updateBudgetById: (id: number, data?: any) => Promise<Budget | null>;
+  deleteBudgetById: (id: number) => Promise<Budget>;
 }
